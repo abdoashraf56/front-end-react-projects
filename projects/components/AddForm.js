f0675234a3cdd7847b8e3ef8c1a621255cec796b
abdo ashraf <abdo.ashraf975.com>
2021-05-11 01:00:02 +0200

    create funcationality for add and filter todos

--- projects/components/AddForm.js
+++ projects/components/AddForm.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
 
-function AddForm({ styles }) {
+function AddForm({ styles, add }) {
+    let [text, setText] = useState('')
+
+    let handleChange = (e => {
+        let value = e.target.value
+        setText(value)
+    })
+
+    let handleClick = () => {
+        add({ id: 0, text, compelete: false })
+        setText('')
+    }
+
     return (
         <div className={styles.flex_row}>
-            <input className={styles.input_form} type="text" maxLength={50} />
-            <button className={styles.add_btn} title={"+"}>+</button>
+            <input
+                className={styles.input_form}
+                type="text" maxLength={50}
+                value={text}
+                onChange={handleChange}
+            />
+            <button
+                className={styles.add_btn}
+                title={"+"}
+                onClick={handleClick}
+            >+</button>
         </div>
     );
 }

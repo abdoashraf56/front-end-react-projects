d0169bc74b27501fce634cc32d49b0b7c3189813
abdo ashraf <abdo.ashraf975.com>
2021-05-16 12:13:03 +0200

    inital point of project

---
+++ pages/gencolor.js
@@ -0,0 +1,11 @@
+import styles from '../styles/gencolor.module.scss'
+
+function gencolor(props) {
+    return (
+        <div>
+            <h1 className={styles.title}>Generate Colors</h1>
+        </div>
+    );
+}
+
+export default gencolor;
